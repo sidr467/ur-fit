b5620e4401081f58dc9abc36c1c9d0fa2a95bda1
srw019 <srw019@uregina.ca>
2025-06-03 09:57:51 -0600

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+
+function App() {
+
+  return (
+    <div>
+      <ButtonUsage/>
+    </div>
+  )
+}
+
+export default App
