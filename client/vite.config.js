4787c1c4be38676ed45605b06897ecdef050351b
Tamunotonye Barlow <teb402@uregina.ca>
2025-06-12 22:01:14 -0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
    server: {
     proxy: {
-      '/api': 'http://localhost:3002', // or your backend port
+      '/api': 'http://localhost:3002',
       '/public': 'http://localhost:3002'
     }
   }
